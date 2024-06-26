9cb45b7027d607141e1ecacc17801fd028ef3aea
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-06-26 12:55:59 +0800

    updated project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import WindowSizeProvider from './WindowSizeProvider';
 import { useSpringCarousel } from 'react-spring-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
@@ -16,11 +16,16 @@ const Projects = () => {
   const mockItems = [
     {
         id: 'item-1',
-        title: 'Kanoah: Web & Mobile-Based Service Finder',
+        title: 'Kanoah: Web-Based Service Finder',
         description : "A web and mobile-based application for searching and booking services based on the user's location, with chat integration.",
-        techStack : ['React', 'React native', 'Tailwind', 'NodeJS', 'ExpressJS', 'MongoDB'],
-        img : [kanoahImg, `${baseLoc}/Kanoah/SS1.png`, `${baseLoc}/Kanoah/SS2.png`, `${baseLoc}/Kanoah/SS3.png`, `${baseLoc}/Kanoah/SS5.png`, `${baseLoc}/Kanoah/SS6.png`,
-        `${baseLoc}/Kanoah/SS7.png`, `${baseLoc}/Kanoah/SS8.png`,  `${baseLoc}/Kanoah/SS9.png`],
+        techStack : ['React', 'Tailwind', 'NodeJS', 'ExpressJS', 'MongoDB'],
+        img : [
+          kanoahImg,
+          'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328953/SS1_hym6xj.png', 'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328952/SS2_fs1dhc.png',
+          'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328953/SS3_qjdw0m.png', 'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328952/SS4_oztgjh.png',
+          'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328953/SS5_ww7jb8.png', 'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328954/SS9_mm9bmr.png', 
+          'https://res.cloudinary.com/dnbgrdgpn/image/upload/v1719328953/SS8_t484fy.png'
+        ],
         video : `${baseLoc}/Kanoah/KanoahVideoDemo.mp4`,
         projectLink : 'https://kanoah-web.vercel.app/'
     },
@@ -51,68 +56,69 @@ const Projects = () => {
     useListenToCustomEvent,
     slideToPrevItem, // go back to previous slide
     slideToNextItem, // move to next slide
-} = useSpringCarousel({
-    itemsPerSlide: 3, // number of slides per view
-    withLoop: true, // will loop
-    initialStartingPosition: 'center', // the active slide will be at the center
-    gutter: 24, // to add the space between slides
-    items: mockItems.map((item) => {
-      return {
-          ...item,
-          renderItem: (
-              <div className={`bg-transparent border border-themeGreen rounded-md flex flex-col gap-2 aspect-[2] mx-5 w-full max-w-full  text-2xl text-white transition-all duration-700 ${currentSlide === item.id ? 'z-10 scale-[1.3] ': ' scale-[0.9] backdrop-blur-sm opacity-25 '}`}>
-                 {/* Image */}
-                 <div className='w-full flex rounded-md overflow-hidden aspect-video'>
-                  <Carousel showIndicators={false} 
-                  showThumbs={false} stopOnHover showStatus={false} autoPlay={currentSlide === item.id ? true : false} infiniteLoop interval={5000}>
-                    {
-                      item.img.map((imgsrc)=>(
-                      <div className='w-full group relative spect-video origin-center '>
-                        <div className='cursor-pointer  '>
-                          <div className='top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] absolute z-40 hidden group-hover:flex space-x-2'>
-                            {
-                              item.video && <button onClick={()=>viewVideo(item.video)} className=' text-xs px-2 pt-1.5 rounded-sm pb-1 bg-themeGreen '>Video demo</button>
-                            }
-                            {
-                              item.projectLink && <button onClick={()=> window.open(item.projectLink, '_blank')} className=' text-xs px-2 pt-1.5 rounded-sm pb-1 bg-themeGreen '>View Project</button>
-                            }
+  } = useSpringCarousel({
+      itemsPerSlide: 3, // number of slides per view
+      withLoop: true, // will loop
+      initialStartingPosition: 'center', // the active slide will be at the center
+      gutter: 24, // to add the space between slides
+      items: mockItems.map((item) => {
+        return {
+            ...item,
+            renderItem: (
+                <div className={`bg-transparent border border-themeGreen rounded-md flex flex-col gap-2 aspect-[2] mx-5 w-full max-w-full  text-2xl text-white transition-all duration-700 ${currentSlide === item.id ? 'z-10 scale-[1.3] ': ' scale-[0.9] backdrop-blur-sm opacity-25 '}`}>
+                  {/* Image */}
+                  <div className='w-full flex rounded-md overflow-hidden aspect-video'>
+                    <Carousel showIndicators={false} 
+                    showThumbs={false} stopOnHover showStatus={false} autoPlay={currentSlide === item.id ? true : false} infiniteLoop interval={5000}>
+                      {
+                        item.img.map((imgsrc)=>(
+                        <div className='w-full group relative spect-video origin-center '>
+                          <div className='cursor-pointer  '>
+                            <div className='top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] absolute z-40 hidden group-hover:flex space-x-2'>
+                              {
+                                item.video && <button onClick={()=>viewVideo(item.video)} className=' text-xs px-2 pt-1.5 rounded-sm pb-1 bg-themeGreen '>Video demo</button>
+                              }
+                              {
+                                item.projectLink && <button onClick={()=> window.open(item.projectLink, '_blank')} className=' text-xs px-2 pt-1.5 rounded-sm pb-1 bg-themeGreen '>View Project</button>
+                              }
+                            </div>
+                            <img className=' cursor-pointer group-hover:brightness-50 w-full h-full object-cover' src={imgsrc} alt="" />
+                            
                           </div>
-                          <img className=' cursor-pointer group-hover:brightness-50 w-full h-full object-cover' src={imgsrc} alt="" />
                           
                         </div>
-                        
-                      </div>
+                        ))
+                      }
+                    </Carousel>
+                  </div>
+                  <h3 className='text-base px-2'>{item.title}</h3>
+                  <p className='text-xs px-2 font-normal text-gray-400'>{item.description}</p>
+
+                  {/* Tech Stack */}
+                  <div className='w-full flex gap-2 flex-wrap px-2 pb-2 mt-5'>
+                    {
+                      item.techStack.map((stack)=>(
+                        <div className='text-extraXs leading-6 px-3 flex items-center font-light border border-themeGreen rounded-full'>
+                          <p className='mt-0.5'>{stack}</p>
+                        </div>
                       ))
                     }
-                  </Carousel>
-                 </div>
-                 <h3 className='text-base px-2'>{item.title}</h3>
-                 <p className='text-xs px-2 font-normal text-gray-400'>{item.description}</p>
+                  </div>
+                </div>
+            )
+        }
+        
+    }),
+    
+    
+  })
 
-                 {/* Tech Stack */}
-                 <div className='w-full flex gap-2 flex-wrap px-2 pb-2 mt-5'>
-                  {
-                    item.techStack.map((stack)=>(
-                      <div className='text-extraXs leading-6 px-3 flex items-center font-light border border-themeGreen rounded-full'>
-                        <p className='mt-0.5'>{stack}</p>
-                      </div>
-                    ))
-                  }
-                 </div>
-              </div>
-          )
-      }
-      
-  }),
-  
-  
-})
+  useListenToCustomEvent((event) => {
+    if (event.eventName === 'onSlideStartChange') {
+        setCurrentSlide(event?.nextItem?.id)
+    }
+  })
 
-useListenToCustomEvent((event) => {
-  if (event.eventName === 'onSlideStartChange') {
-      setCurrentSlide(event?.nextItem?.id)
-  }
-})
 
 
 
